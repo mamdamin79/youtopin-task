a5b37a65fca1039249483f79eb1d0eb6ee094df0
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2023-07-04 09:20:26 -0700

    add json server as a backend endpoint on port 3001 - create a basic todosSlice file

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [];
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const {} = todosSlice.actions
+
+export default todosSlice.reducer
